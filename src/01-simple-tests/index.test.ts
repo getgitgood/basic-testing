383ea6dccc5c2cb04461a3bff9046c08102e990c
Nikolai Shvechkov <vivalastelmah@gmail.com>
2025-05-10 16:22:36 +0300

    refactor: simple tests

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,31 +1,24 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
-
 import { Action, simpleCalculator } from '01-simple-tests';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
     expect(simpleCalculator({ a: 5, b: 5, action: Action.Add })).toBe(10);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
     expect(simpleCalculator({ a: 15, b: 5, action: Action.Subtract })).toBe(10);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
     expect(simpleCalculator({ a: 5, b: 5, action: Action.Multiply })).toBe(25);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
     expect(simpleCalculator({ a: 5, b: 5, action: Action.Divide })).toBe(1);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
     expect(simpleCalculator({ a: 2, b: 2, action: Action.Exponentiate })).toBe(
       4,
     );
